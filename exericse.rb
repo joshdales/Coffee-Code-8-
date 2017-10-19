df2d88c3a6d753964c75c3c425f5ffec73443fd4
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-19 09:16:52 -0400

    Convert arrays to hash

---
+++ exericse.rb
@@ -0,0 +1,12 @@
+movies = [
+  ['Alfonso Cuaron',  'Gravity'],
+  ['Spike Jonze',     'Her'],
+  ['Martin Scorsese', 'The Wolf of Wall Street']
+]
+
+movie_hash = {}
+movies.each do |k, h|
+movie_hash[k] = h
+ end
+
+p movie_hash
