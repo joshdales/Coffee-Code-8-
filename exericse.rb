6542d2ed4a9e80b3e59e5e6c875741c02168aab7
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-19 09:27:31 -0400

    Inject method to create hash

--- exericse.rb
+++ exericse.rb
@@ -5,8 +5,31 @@ movies = [
 ]
 
 movie_hash = {}
-movies.each do |k, h|
-movie_hash[k] = h
- end
 
-p movie_hash
+movies.each do |k, h|
+  movie_hash[k] = h
+end
+
+puts movie_hash
+
+# =========> INJECT
+numbers = [4, 8, 2, 6]
+
+sum = numbers.inject(0) do |result, num|
+  intermediate_result = result + num
+  intermediate_result
+end
+# Iteration|result|num
+# 0th      |0     |4
+# 1st      |4     |8
+# 2nd      |8     |12
+# 3rd      |2     |14
+# 4th      |6     |20
+puts sum
+ # ========
+
+ imdb = movies.inject({}) do |result, (director, film)| # => |result, movie|
+   result[director] = film # => result[movie[0]] = movie[1]
+   result
+end
+puts imdb
