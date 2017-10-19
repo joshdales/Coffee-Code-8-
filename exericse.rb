4c035ce54d96b9850920341fe37dbaacb8bbbcab
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-19 09:53:09 -0400

    Use regular expression to find valid phone numbers

--- exericse.rb
+++ exericse.rb
@@ -33,3 +33,17 @@ puts sum
    result
 end
 puts imdb
+
+
+
+def valid_phone_number(phone_number)
+  if phone_number =~ /\A\(\d{3}\)\s\d{3}\-\d{4}\z/
+    return true
+  else
+    return false
+  end
+end
+
+puts valid_phone_number("(123) 456-7890") # => returns true
+puts valid_phone_number("(1111)555 2345") # => returns false
+puts valid_phone_number("(098) 123 4567") # => returns false
